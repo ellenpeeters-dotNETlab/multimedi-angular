2ee28eb627fd5fde93e23726f70e54c764f6a050
Ellen Peeters <ellen.peeters@dotnetlab.eu>
2021-05-07 08:59:06 +0200

    display messages with for

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  public messages = [];
+  private message = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.';
+  private author = 'John Smith';
+  private amount = _.random(1, 10);
 
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 1; i <= this.amount; i++) {
+      this.messages.push(
+        {
+          author: this.author,
+          message: this.message
+        }
+      );
+    }
   }
-
 }
